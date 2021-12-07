93d28262036d85e903a6735338981fd7e1cdd829
Hanan Neor <hananneor@gmail.com>
2021-12-07 13:44:50 +0200

    scrollbar

--- TodoApp/Commands/ToggleTodoComposerCommand.cs
+++ TodoApp/Commands/ToggleTodoComposerCommand.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TodoApp.ViewModel;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using TodoApp.ViewModel;
 
-namespace TodoApp.Commands
-{
-    internal class ToggleTodoComposerCommand : CommandBase
-    {
-        StateViewModel _stateViewModel;
-        public ToggleTodoComposerCommand(StateViewModel stateViewModel)
-        {
-            _stateViewModel = stateViewModel;
-            //_todoViewModel.PropertyChanged += OnViewModelProperyChanged;
-        }
-        public override void Execute(object? parameter)
-        {
-                ToggleComposer();
-        }
-        private void ToggleComposer()
-        {
-            _stateViewModel.ShowComposer = !_stateViewModel.ShowComposer;
-        }
-    }
-}
+//namespace TodoApp.Commands
+//{
+//    internal class ToggleTodoComposerCommand : CommandBase
+//    {
+//        StateViewModel _stateViewModel;
+//        public ToggleTodoComposerCommand(StateViewModel stateViewModel)
+//        {
+//            _stateViewModel = stateViewModel;
+//            //_todoViewModel.PropertyChanged += OnViewModelProperyChanged;
+//        }
+//        public override void Execute(object? parameter)
+//        {
+//                ToggleComposer();
+//        }
+//        private void ToggleComposer()
+//        {
+//            _stateViewModel.ShowComposer = !_stateViewModel.ShowComposer;
+//        }
+//    }
+//}
--- TodoApp/ViewModel/StateViewModel.cs
+++ TodoApp/ViewModel/StateViewModel.cs
@@ -1,29 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using TodoApp.Commands;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Input;
+//using TodoApp.Commands;
 
-namespace TodoApp.ViewModel
-{
-    internal class StateViewModel : ViewModelBase
-    {
-        private bool _showComposer = false;
-        public bool ShowComposer
-        {
-            get { return _showComposer; }
-            set
-            {
-                _showComposer = value;
-                OnPropertyChanged(nameof(ShowComposer));
-            }
-        }
+//namespace TodoApp.ViewModel
+//{
+//    internal class StateViewModel : ViewModelBase
+//    {
+//        private bool _showComposer = false;
+//        public bool ShowComposer
+//        {
+//            get { return _showComposer; }
+//            set
+//            {
+//                _showComposer = value;
+//                OnPropertyChanged(nameof(ShowComposer));
+//            }
+//        }
 
-        public ICommand ToggleTodoComposerCommand
-        {
-            get { return new ToggleTodoComposerCommand(this); }
-        }
-    }
-}
+//        public ICommand ToggleTodoComposerCommand
+//        {
+//            get { return new ToggleTodoComposerCommand(this); }
+//        }
+//    }
+//}
